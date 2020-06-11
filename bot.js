67f8eeae73a993be65a941faeae11924a59ce3ca
LmmAamz <66748875+LmmAamz@users.noreply.github.com>
2020-06-11 16:26:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['ID'];
+const myid = ['556463469029752833'];
 const prefix = ['$'];
 const client = new Discord.Client();
 const client2 = new Discord.Client();
